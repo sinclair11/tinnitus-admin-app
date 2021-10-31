7829c0757f175880b0a2b2e5b8f1638c527a68b5
Ștefan Țîncu <stefantnc11@gmail.com>
2021-11-01 00:32:57 +0200

    TNS-32: Move modal

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
-import Modal from 'react-modal';
-import { dialogStyles } from '@src/styles/styles';
 import { Button } from 'react-bootstrap';
 import { Icons } from '@utils/icons';
 
 type DialogProps = {
-	isOpen: boolean;
 	setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
 	message: string;
 };
 
 export const Dialog: React.FC<DialogProps> = (props: DialogProps) => {
 	return (
-		<Modal style={dialogStyles} isOpen={props.isOpen} ariaHideApp={false}>
+		<>
 			<div className="DialogHeader">
 				<p style={{ margin: '4px' }}>Message</p>
 			</div>
@@ -28,6 +25,6 @@ export const Dialog: React.FC<DialogProps> = (props: DialogProps) => {
 				className="CancelIcon"
 				onClick={(): void => props.setIsOpen(false)}
 			/>
-		</Modal>
+		</>
 	);
 };
