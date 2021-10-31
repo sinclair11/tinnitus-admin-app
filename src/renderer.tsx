ef69103d1232ba22671927a83960001c2fbf0b65
Ștefan Țîncu <stefantnc11@gmail.com>
2021-11-01 00:30:47 +0200

    TNS-32: Add store provider

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { inDev } from './utils/helpers';
+import { Provider } from 'react-redux';
+import { store } from '@src/store/store';
 
 // Say something
 console.log('[ERWT] : Renderer execution started');
@@ -10,7 +12,10 @@ console.log('[ERWT] : Renderer execution started');
 const app = <App title="TinnitusAdmin" version="1.0.0" />;
 
 // Render application in DOM
-ReactDOM.render(app, document.getElementById('root'));
+ReactDOM.render(
+	<Provider store={store}>{app}</Provider>,
+	document.getElementById('root'),
+);
 
 // Hot module replacement
 if (inDev() && module.hot) module.hot.accept();
