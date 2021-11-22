a4e69dabf09737afedf9a4ec626ec87919566fb6
Ștefan Țîncu <stefantnc11@gmail.com>
2021-11-23 00:45:23 +0200

    TNS-35: Changes in styles

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -83,7 +83,7 @@ export const hourglassStyle = {
 export const tableStyles = {
 	content: {
 		width: '580px',
-		height: '700px',
+		height: '720px',
 		display: 'inline-block',
 		justifyContent: 'center',
 		alignItems: 'center',
