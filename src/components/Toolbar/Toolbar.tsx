f63f1761b99565f44361862007ce2f5e773f2921
Ștefan Țîncu <stefantnc11@gmail.com>
2021-11-26 15:57:15 +0200

    TNS-42: Complete delete operation

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ToolbarIcons } from '@utils/icons';
 import ReactTooltip from 'react-tooltip';
 import { UploadVideoModal } from '@components/upload/Upload';
 import { store } from '@store/store';
 import { getAuth, ResponseCodes } from '@src/utils/utils';
-import axios, { AxiosError } from 'axios';
+import axios from 'axios';
 import Modal from 'react-modal';
-import { Dialog } from '../dialog/Dialog';
+import { MessageBox } from '../messagebox/messagebox';
+import { DialogBox } from '../dialogbox/dialogbox';
 import { dialogStyles, hourglassStyle } from '@src/styles/styles';
+import { useDispatch } from 'react-redux';
 
 type ToolbarProps = {
 	container: string;
@@ -21,9 +23,20 @@ export const Toolbar: React.FC<ToolbarProps> = (props?: ToolbarProps) => {
 		tags: '',
 		description: '',
 	});
-	const [dialogOpen, setDialogOpen] = useState(false);
-	const [dialogMsg, setDialogMsg] = useState('');
+	const [messageOpen, setMessageOpen] = useState(false);
+	const [messageboxMsg, setMessageboxMsg] = useState('');
 	const [loading, setLoading] = useState(false);
+	const [dialogbox, setDialogbox] = useState(false);
+	const [accept, setAccept] = useState(false);
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		if (accept == true) {
+			requestResourceDeletion();
+			//Reset action accept state
+			setAccept(false);
+		}
+	}, [accept]);
 
 	async function openModal(type: string): Promise<void> {
 		if (type === 'edit') {
@@ -62,13 +75,13 @@ export const Toolbar: React.FC<ToolbarProps> = (props?: ToolbarProps) => {
 					//Info could not be retrieved from database
 					const message = ResponseCodes.get(error.response.status);
 					//Notify user about occured error
-					setDialogMsg(message);
-					setDialogOpen(true);
+					setMessageboxMsg(message);
+					setMessageOpen(true);
 				}
 			} else {
 				//Notify user that he must select a resource first
-				setDialogMsg('Selectati o resursa intai!');
-				setDialogOpen(true);
+				setMessageboxMsg('Selectati o resursa intai!');
+				setMessageOpen(true);
 			}
 		} else if (type === 'upload') {
 			//Make sure editable data is clean
@@ -80,6 +93,51 @@ export const Toolbar: React.FC<ToolbarProps> = (props?: ToolbarProps) => {
 			//Just open the upload modal
 			setModalType(type);
 			setIsOpen(true);
+		}
+	}
+
+	/**
+	 *
+	 */
+	async function requestResourceDeletion(): Promise<void> {
+		//Activate loading screen
+		setLoading(true);
+		try {
+			//Request deletion of resource
+			await axios({
+				method: 'delete',
+				url: `http://127.0.0.1:3000/api/admin/videos?id=${
+					store.getState().resdataReducer.selected
+				}`,
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: `Bearer ${await getAuth()}`,
+				},
+			});
+			//Reset selected resource state
+			dispatch({ type: 'resdata/selected', payload: '' });
+			//Close loading screen
+			setLoading(false);
+		} catch (error) {
+			//Close loading screen
+			setLoading(false);
+			//Retrieve message for corresponding status code
+			const message = ResponseCodes.get(error.response.status);
+			//Set message and notify user about occured error
+			setMessageboxMsg(message);
+			setMessageOpen(true);
+		}
+	}
+
+	function onRequestDeleteClick(): void {
+		//First check if a resource was selected
+		if (store.getState().resdataReducer.selected != '') {
+			setDialogbox(true);
+			console.log(store.getState().resdataReducer.selected);
+		} else {
+			//Notify user that he must select a resource first
+			setMessageboxMsg('Selectati o resursa intai!');
+			setMessageOpen(true);
 		}
 	}
 
@@ -107,6 +165,7 @@ export const Toolbar: React.FC<ToolbarProps> = (props?: ToolbarProps) => {
 				data-tip="Sterge"
 				src={ToolbarIcons['DeleteIcon']}
 				className="ActionIcon"
+				onClick={onRequestDeleteClick}
 			/>
 			<UploadVideoModal
 				modalIsOpen={modalIsOpen}
@@ -115,12 +174,27 @@ export const Toolbar: React.FC<ToolbarProps> = (props?: ToolbarProps) => {
 				editable={editData}
 			/>
 			<Modal
-				isOpen={dialogOpen}
+				isOpen={messageOpen}
 				style={dialogStyles}
 				contentLabel="Upload"
 				ariaHideApp={false}
 			>
-				<Dialog setIsOpen={setDialogOpen} message={dialogMsg} />
+				<MessageBox
+					setIsOpen={setMessageOpen}
+					message={messageboxMsg}
+				/>
+			</Modal>
+			<Modal
+				isOpen={dialogbox}
+				style={dialogStyles}
+				contentLabel="Dialog box"
+				ariaHideApp={false}
+			>
+				<DialogBox
+					setIsOpen={setDialogbox}
+					message="Esti sigur ca vrei sa stergi aceasta resursa?"
+					setAccepted={setAccept}
+				/>
 			</Modal>
 			<Modal isOpen={loading} style={hourglassStyle} ariaHideApp={false}>
 				<div className="hourglass"></div>
