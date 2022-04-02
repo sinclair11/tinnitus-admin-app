7ac1befed3ea942250a144cfebeb846e9e39c96f
Ștefan Țîncu <stefantnc11@gmail.com>
2022-04-02 11:52:49 +0300

    Keep up to date

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,15 @@ import './app.sass';
 import { Login } from '@src/components/login/login';
 import { AudioView } from '@components/audio/audio';
 import UploadView from './components/upload/upload';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { app } from '@src/config/firebase';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const App: React.FC<{ title: string; version: string }> = (props: {
     title: string;
     version: string;
 }) => {
+    app;
     return (
         <Router>
             <Route exact path="/" component={Login} />
