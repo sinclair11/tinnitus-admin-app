0d57852f6a67fdeb5eb0a19adeda8454cf1c33f4
Ștefan Țîncu <stefantnc11@gmail.com>
2022-04-02 11:51:03 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
-import { store } from './store/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import reduxPersist from '@store/reducers/persist';
 
 // Application to Render
 const app = <App title="TinnitusAdmin" version="1.0.0" />;
+const persist = reduxPersist();
 
 // Render application in DOM
 ReactDOM.render(
-    <Provider store={store}>{app}</Provider>,
+    <Provider store={persist.store}>
+        <PersistGate loading={null} persistor={persist.persistor}>
+            {app}
+        </PersistGate>
+    </Provider>,
     document.getElementById('root'),
 );
