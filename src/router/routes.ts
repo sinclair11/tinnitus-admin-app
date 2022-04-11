4d555d6e6f41d986472c7ae37b184215dae78be9
Ștefan Țîncu <stefantnc11@gmail.com>
2022-04-12 01:43:26 +0300

    Update routes.ts
    Resolves: #13

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,10 +1,10 @@
 export const routes = {
     HOME: '/',
     LOGIN: '/login',
-    ALBUM_VIEW: '/album/:id',
-    ALBUM_EDIT: '/album/:id/edit',
+    ALBUM_VIEW: '/album/view/:id',
+    ALBUM_EDIT: '/album/edit/:id',
     ALBUM_CREATE: '/album/create',
-    GENERATOR_VIEW: '/generator/:id',
-    GENERATOR_EDIT: '/generator/:id/edit',
+    GENERATOR_VIEW: '/generator/view/:id',
+    GENERATOR_EDIT: '/generator/edit/:id/',
     GENERATOR_CREATE: '/generator/create',
 };
