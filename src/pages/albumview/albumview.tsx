28472edc2c5decf336259c0aa1964f5fd9a1930a
Ștefan Țîncu <stefantnc11@gmail.com>
2022-04-15 23:09:55 +0300

    Update albumview.tsx
    - Display components based on album id
    - Fetch data for current album using id
    - Fetch song url for player
    
    Resolves: #12

--- src/pages/albumview/albumview.tsx
+++ src/pages/albumview/albumview.tsx
@@ -1,4 +1,4 @@
-import React, { createRef, useEffect, useRef } from 'react';
+import React, { createRef, useEffect, useRef, useState } from 'react';
 import { Container } from 'react-bootstrap';
 import SearchBar from '@src/components/searchbar/searchbar';
 import Toolbar from '@src/components/toolbar/toolbar';
@@ -7,10 +7,15 @@ import { CombinedStates } from '@src/store/reducers/custom';
 import Sidebar from '@components/sidebar/sidebar';
 import { useHistory, useParams } from 'react-router-dom';
 import { getAuth } from 'firebase/auth';
-import { app } from '@config/firebase';
-import { SongData } from '@src/types/album';
+import { app, db } from '@config/firebase';
 import Artwork from '@components/artwork/artwork';
+import AlbumInfoView from '@components/albuminfo/albuminfo';
 import '@components/modal-search/modal-search.css';
+import { AlbumInfo, SongData } from '@src/types/album';
+import { Table } from '@components/table/table';
+import { Icons } from '@src/utils/icons';
+import { doc, getDoc, collection } from 'firebase/firestore';
+import Player from '@components/player/player';
 
 type AlbumViewProps = {
     location: any;
@@ -18,59 +23,117 @@ type AlbumViewProps = {
 
 const AlbumView: React.FC<AlbumViewProps> = (props: AlbumViewProps) => {
     const { id } = useParams<{ id: string }>();
-    const albumData = useRef();
+    const [albumData, setAlbumData] = useState(null);
+    const [dataFetched, setDataFetched] = useState(false);
     const auth = useSelector<CombinedStates>((state) => state.generalReducer.auth) as any;
     const history = useHistory();
     const searchbarRef = createRef<any>();
+    const playerRef = createRef<any>();
+    const container = useRef(null);
 
     useEffect(() => {
         if (auth) {
+            if (id !== '0') {
+                setDataFetched(false);
+                //Load data for selected album
+                fetchAlbumData(id);
+            }
         } else {
             history.push('/login');
         }
-    }, [getAuth(app).currentUser]);
+    }, [getAuth(app).currentUser, id]);
 
     useEffect(() => {
-        //Continue in page
-        if (id !== '0') {
-            //Load data for selected album
-            albumData.current = props.location.state;
-        } else {
-            //Reset value of searchbar
-            searchbarRef.current.setSearchValue('');
+        if (dataFetched) {
+            getSongUrl(albumData.songs[0]);
         }
-    }, [id]);
+    }, [dataFetched]);
 
-    function displayContent(): JSX.Element {
-        if (id === '0') {
-            return (
-                <div className="section-no-content">
-                    <p>Please select an album to see more information</p>
-                </div>
-            );
-        } else {
-            return (
-                <div className="section-album-content">
-                    {id !== '0' ? <Artwork type="view" img={props.location.state.artwork} /> : null}
-                </div>
-            );
+    async function fetchAlbumData(id: string): Promise<void> {
+        try {
+            const docRef = doc(collection(db, 'albums'), id);
+            const docRes = await getDoc(docRef);
+            const reqRef = doc(collection(db, 'misc'), 'oci-req');
+            const docReq = await getDoc(reqRef);
+            const data = docRes.data();
+            data.artwork = `${docReq.data().value}${id}/artwork.${data.ext}`;
+            data.upload_date = data.upload_date.toDate().toDateString();
+            setAlbumData(data);
+            setDataFetched(true);
+        } catch (error) {
+            console.log(error);
         }
     }
 
-    return (
-        <div className="page">
-            <Sidebar />
-            <div className="section-album">
-                <div className="SearchBarDiv">
-                    <SearchBar type="album" ref={searchbarRef} />
+    async function getSongUrl(song: SongData): Promise<void> {
+        try {
+            const reqRef = doc(collection(db, 'misc'), 'oci-req');
+            const docReq = await getDoc(reqRef);
+            playerRef.current.setSong(`${docReq.data().value}${id}/${song.name}.${song.extension}`);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    function displayPage(): JSX.Element {
+        if (id === '0') {
+            return (
+                <div className="page">
+                    <Sidebar />
+                    <div className="section-album" ref={container}>
+                        <div className="SearchBarDiv">
+                            <SearchBar type="album" ref={searchbarRef} />
+                        </div>
+                        <Container>
+                            <div className="section-no-content">
+                                <p>Please select an album to see more information</p>
+                            </div>
+                        </Container>
+                    </div>
                 </div>
-                <Container id="content" className="ContentPlaceholder">
-                    <Toolbar type="audio" />
-                    {displayContent()}
-                </Container>
-            </div>
-        </div>
-    );
+            );
+        } else {
+            if (dataFetched && albumData !== undefined) {
+                return (
+                    <div className="page">
+                        <Sidebar />
+                        <div className="section-album" ref={container}>
+                            <div className="SearchBarDiv">
+                                <SearchBar type="album" ref={searchbarRef} />
+                            </div>
+                            <Container>
+                                <Toolbar type="audio" />
+                                <div className="section-album-content">
+                                    <div>
+                                        <Artwork type="view" img={albumData.artwork} />
+                                        <Player ref={playerRef} />
+                                    </div>
+                                    <AlbumInfoView data={albumData} />
+                                </div>
+                                <Table
+                                    type="view"
+                                    headers={[
+                                        'Name',
+                                        'Position',
+                                        'Length',
+                                        'Category',
+                                        <img data-tip="Views" data-delay-show="500" src={Icons.Views} />,
+                                        <img data-tip="Likes" data-delay-show="500" src={Icons.Likes} />,
+                                        <img data-tip="Favorites" data-delay-show="500" src={Icons.Favorites} />,
+                                    ]}
+                                    data={albumData.songs}
+                                />
+                            </Container>
+                        </div>
+                    </div>
+                );
+            } else {
+                return <div className="page"></div>;
+            }
+        }
+    }
+
+    return displayPage();
 };
 
 export default AlbumView;
