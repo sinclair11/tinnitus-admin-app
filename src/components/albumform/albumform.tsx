342d8d6ac036eae472e6ca2297b79a62e9f94f22
Ștefan Țîncu <stefantnc11@gmail.com>
2022-04-17 20:26:28 +0300

    Add handling for edit page

--- src/components/albumform/albumform.tsx
+++ src/components/albumform/albumform.tsx
@@ -1,30 +1,37 @@
 import { db } from '@src/config/firebase';
 import { collection, getDocs, query, where } from 'firebase/firestore';
-import React, { forwardRef, useState, useImperativeHandle, useRef } from 'react';
+import React, { forwardRef, useState, useImperativeHandle, useRef, useEffect } from 'react';
 import { InputGroup, FormControl } from 'react-bootstrap';
 import Dropdown from '@components/dropdown/dropdown';
-import { getDurationFormat } from '@src/utils/helpers';
+import { getDurationFormat, parseTags } from '@utils/helpers';
 import { AlbumFormData } from '@src/types/album';
+import { useSelector } from 'react-redux';
 
 type FormProps = {
     type: string;
-    name?: string;
-    description?: string;
-    tags?: string;
-    length?: string;
-    category?: string;
+    data?: AlbumFormData;
 };
 
 const AlbumForm = forwardRef((props: FormProps, ref?: any) => {
+    const categories = useSelector<any>((state) => state.generalReducer.categories) as string[];
     const [name, setName] = useState('');
     const [nameinvalid, setNameInvalid] = useState('');
     const [description, setDescription] = useState('');
     const [descInvalid, setDescInvalid] = useState('');
     const [notification, setNotification] = useState('');
-    const [categories, setCategories] = useState([]);
+    const [category, setCategory] = useState(categories[0]);
     const [tags, setTags] = useState('');
     const [length, setLength] = useState('');
-    const category = useRef('');
+
+    useEffect(() => {
+        if (props.data !== undefined && props.type === 'edit') {
+            setName(props.data.name);
+            setDescription(props.data.description);
+            setTags(parseTags('string', props.data.tags));
+            setLength(props.data.length);
+            setCategory(props.data.category);
+        }
+    }, []);
 
     useImperativeHandle(ref, () => ({
         getData: (): AlbumFormData => {
@@ -32,16 +39,11 @@ const AlbumForm = forwardRef((props: FormProps, ref?: any) => {
                 name: name,
                 description: description,
                 ext: '',
-                tags: parseTags(),
+                tags: parseTags('array', tags),
                 length: length,
                 notification: notification,
-                category: category.current,
+                category: category,
             };
-        },
-
-        setCategories: (categories: Array<string>): void => {
-            setCategories(categories);
-            category.current = categories[0];
         },
 
         setTotalDuration: (value: Array<string>): void => {
@@ -54,7 +56,7 @@ const AlbumForm = forwardRef((props: FormProps, ref?: any) => {
             setTags('');
             setLength('');
             setNotification('');
-            category.current = categories[0];
+            setCategory(categories[0]);
         },
 
         getInputValidation: async (): Promise<boolean> => {
@@ -64,13 +66,28 @@ const AlbumForm = forwardRef((props: FormProps, ref?: any) => {
                 setNameInvalid('This field is mandatory');
                 retVal++;
             } else {
-                const q = query(collection(db, 'albums'), where('name', '==', name));
-                const querySnapshot = await getDocs(q);
-                if (querySnapshot.docs.length > 0) {
-                    setNameInvalid('An album with this name already exists');
-                    retVal++;
-                } else {
-                    setNameInvalid('');
+                //Different behaviour depending on the type
+                if (props.type === 'edit') {
+                    //Check if new provided name is different from the old one
+                    if (name !== props.data.name) {
+                        const q = query(collection(db, 'albums'), where('name', '==', name));
+                        const querySnapshot = await getDocs(q);
+                        if (querySnapshot.docs.length > 0) {
+                            setNameInvalid('An album with this name already exists');
+                            retVal++;
+                        } else {
+                            setNameInvalid('');
+                        }
+                    }
+                } else if (props.type === 'create') {
+                    const q = query(collection(db, 'albums'), where('name', '==', name));
+                    const querySnapshot = await getDocs(q);
+                    if (querySnapshot.docs.length > 0) {
+                        setNameInvalid('An album with this name already exists');
+                        retVal++;
+                    } else {
+                        setNameInvalid('');
+                    }
                 }
             }
             if (description === '') {
@@ -88,34 +105,7 @@ const AlbumForm = forwardRef((props: FormProps, ref?: any) => {
     }));
 
     function onCategoryChange(value: string): void {
-        category.current = value;
-    }
-
-    function parseTags(): Array<string> {
-        const parsedTags = new Array<string>();
-        const hashIndexes = new Array<number>();
-
-        //Get indexes of all hash characters
-        for (let i = 0; i < tags.length; i++) {
-            if (tags[i] === '#') {
-                hashIndexes.push(i);
-            }
-        }
-
-        //Extract hash tags
-        for (let i = 0; i < hashIndexes.length - 1; i++) {
-            for (let j = hashIndexes[i] + 1; j < tags.length; j++) {
-                if (tags[j] === '#') {
-                    parsedTags.push(tags.slice(hashIndexes[i], j).trim());
-                    break;
-                }
-            }
-        }
-
-        //Last hash tag goes until the eng of string
-        parsedTags.push(tags.slice(hashIndexes[hashIndexes.length - 1]).trim());
-
-        return parsedTags;
+        setCategory(value);
     }
 
     function calculateTotalLength(songs: Array<string>): number {
@@ -153,7 +143,7 @@ const AlbumForm = forwardRef((props: FormProps, ref?: any) => {
                     items={categories}
                     className="dropdown-margin"
                     onChange={onCategoryChange}
-                    current={props.category != undefined ? props.category : categories[0]}
+                    current={category}
                 />
             </div>
             <InputGroup hasValidation className="input-group input-group-area">
@@ -181,16 +171,18 @@ const AlbumForm = forwardRef((props: FormProps, ref?: any) => {
                     onChange={(event): void => setTags(event.target.value)}
                 />
             </InputGroup>
-            <InputGroup hasValidation className="input-group input-group-area">
-                <InputGroup.Text className="label">Notification (optional)</InputGroup.Text>
-                <FormControl
-                    className="input-notification"
-                    required
-                    as="textarea"
-                    value={notification}
-                    onChange={(event): void => setNotification(event.target.value)}
-                />
-            </InputGroup>
+            {props.type === 'create' ? (
+                <InputGroup hasValidation className="input-group input-group-area">
+                    <InputGroup.Text className="label">Notification (optional)</InputGroup.Text>
+                    <FormControl
+                        className="input-notification"
+                        required
+                        as="textarea"
+                        value={notification}
+                        onChange={(event): void => setNotification(event.target.value)}
+                    />
+                </InputGroup>
+            ) : null}
             <p className="album-total-duration">Duration: {length}</p>
         </div>
     );
