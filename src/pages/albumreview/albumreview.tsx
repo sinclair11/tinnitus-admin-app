5de666a723563b3e337b4f796ea5281f1df08f8c
Ștefan Țîncu <stefantnc11@gmail.com>
2022-04-26 00:35:46 +0300

    Configure and generate PWA

--- src/pages/albumreview/albumreview.tsx
+++ src/pages/albumreview/albumreview.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import ReviewView from '@components/reviews/reviews';
+import Sidebar from '@src/components/sidebar/sidebar';
 
 const AlbumReviews: React.FC = () => {
     const { id } = useParams<{ id: string }>();
 
     return (
         <div className="page" id="page-upload-edit">
+            <Sidebar />
             <div className="album-review">
                 <ReviewView id={id} />
             </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig, UserConfigExport } from 'vite'
-import { resolve } from 'path'
-import react from '@vitejs/plugin-react'
-import alias from '@rollup/plugin-alias'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import react from '@vitejs/plugin-react';
+import alias from '@rollup/plugin-alias';
+import { VitePWA } from 'vite-plugin-pwa';
 
 const projectRootDir = resolve(__dirname);
 
@@ -9,49 +10,93 @@ const projectRootDir = resolve(__dirname);
  * @description Development configuration
  */
 const devConfig = {
-	plugins: [react(), alias()],
-	mode: 'development',
-	resolve: {
-		alias: {
-			'@src': resolve(projectRootDir, 'src'),
-			'@components': resolve(projectRootDir, 'src/components'),
+    plugins: [react(), alias()],
+    mode: 'development',
+    resolve: {
+        alias: {
+            '@src': resolve(projectRootDir, 'src'),
+            '@components': resolve(projectRootDir, 'src/components'),
             '@pages': resolve(projectRootDir, 'src/pages'),
             '@services': resolve(projectRootDir, 'src/services'),
-			'@config': resolve(projectRootDir, 'src/config'),
-			'@utils': resolve(projectRootDir, 'src/utils'),
-			'@store': resolve(projectRootDir, 'src/store'),
-			'@icons': resolve(projectRootDir, 'src/icons'),
-		}
-	}
+            '@config': resolve(projectRootDir, 'src/config'),
+            '@utils': resolve(projectRootDir, 'src/utils'),
+            '@store': resolve(projectRootDir, 'src/store'),
+            '@icons': resolve(projectRootDir, 'src/icons'),
+        },
+    },
 };
 
 /**
  * @description Production configuration
  */
 const prodConfig = {
-	plugins: [react(), alias()],
-	mode: 'production',
-	build: {
-		outDir: 'build'
-	},
-	resolve: {
-		alias: {
-			'@src': resolve(projectRootDir, 'src'),
-			'@components': resolve(projectRootDir, 'src/components'),
+    plugins: [
+        react(),
+        alias(),
+        VitePWA({
+            includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+            manifest: {
+                name: 'Tinnitus CMS',
+                short_name: 'CMS',
+                description:
+                    'Content management system for Tinnitus mobile application to manage content and resources. Other features are statistics and analytics of application usage, users, subscriptions etc.',
+                display: 'standalone',
+                background_color: '#0a2351',
+                theme_color: '#0a2351',
+                dir: 'ltr',
+                icons: [
+                    {
+                        src: 'android-chrome-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'android-chrome-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'apple-touch-icon.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any maskable',
+                    },
+                ],
+            },
+        }),
+    ],
+    mode: 'production',
+    build: {
+        outDir: 'build',
+    },
+    resolve: {
+        alias: {
+            '@src': resolve(projectRootDir, 'src'),
+            '@components': resolve(projectRootDir, 'src/components'),
             '@pages': resolve(projectRootDir, 'src/pages'),
             '@services': resolve(projectRootDir, 'src/services'),
-			'@config': resolve(projectRootDir, 'src/config'),
-			'@utils': resolve(projectRootDir, 'src/utils'),
-			'@store': resolve(projectRootDir, 'src/store'),
-			'@icons': resolve(projectRootDir, 'src/icons'),
-		}
-	}
+            '@config': resolve(projectRootDir, 'src/config'),
+            '@utils': resolve(projectRootDir, 'src/utils'),
+            '@store': resolve(projectRootDir, 'src/store'),
+            '@icons': resolve(projectRootDir, 'src/icons'),
+        },
+    },
 };
 
 export default defineConfig(({ command }) => {
-	if (command === 'build') {
-		return prodConfig;
-	} else if (command === 'serve') {
-		return devConfig;
-	}
-})
+    if (command === 'build') {
+        return prodConfig;
+    } else if (command === 'serve') {
+        return devConfig;
+    }
+});
